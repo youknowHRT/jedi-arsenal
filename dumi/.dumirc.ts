15bcb4930747b9d879e3f9c93ce1c1507ccd90d9
youknowHRT <1021300769@qq.com>
2024-06-16 16:47:36 +0800

    .

--- dumi/.dumirc.ts
+++ dumi/.dumirc.ts
@@ -12,11 +12,9 @@ export default defineConfig({
   resolve: {
     docDirs: ["docs"],
     atomDirs: [
-      // 在这里修改components的匹配路径
       { type: "hooks", dir: "../packages/hooks/src" },
       { type: "components", dir: "../packages/components/src" },
     ]
-    // codeBlockMode: 'passive'
   },
   themeConfig: {
     name: "Jedi Arsenal",
@@ -33,6 +31,5 @@ export default defineConfig({
   },
   alias: {
     '@jedi-arsenal/hooks': path.join(__dirname, '../packages/hooks/src/index.ts'),
-    // '@jedi-arsenal/hooks': process.cwd() + '/packages/hooks/src/index.ts',
   }
 });
